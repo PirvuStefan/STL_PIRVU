7503e8b8ba9b900eeb745dae5a33ef8e62542fde
pirvu <117602013+PirvuStefan@users.noreply.github.com>
2024-11-12 16:17:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,40 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-
+#include <cstring>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-int main()
-{
-    ifstream inFile("input.txt");
-
-    int no_problems, no_doctors;
-    string name, speciality;
+int main() {
+    int probleme, j;
+    cin >> probleme;
     
-    inFile >> no_problems;
-
-    for (int i = 0; i < no_problems; i++)
-    {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+    vector<string> a(101), b(101);
+    
+    for(int i = 1; i <= probleme; i++){
+        cin >> a[i]; // boli
+        cin >> b[i]; // specializare
     }
-
-    inFile >> no_doctors;
-
-    for (int i = 0; i < no_doctors; i++)
-    {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+    
+    int doctori;
+    cin >> doctori;
+    for(int i = 1; i <= doctori; i++){
+        string id, specializare;
+        cin >> id >> specializare;
+        
+        bool accepted = false;
+        for(j = 1; j <= probleme; j++){
+            if(specializare == b[j]){
+                cout << a[j] << " Acceptat" << endl;
+                accepted = true;
+                break;
+            }
+        }
+        
+        if (!accepted) {
+            cout << id << " Respins" << endl;
+        }
     }
-
+    
     return 0;
 }
+
